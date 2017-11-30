494367127e4ff5b407d05450715a3c78ab091afe
Andrés Carreño <hello@andrescarreno.co>
2017-11-29 19:16:48 -0500

    Removing additional instance of the Gallery component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { Gallery } from './gallery/gallery.js'
 import data from './gallery/galleryData.js'
 
 /* eslint-disable */
-new Gallery(document.querySelector('.gallery-one'), data)
-new Gallery(document.querySelector('.gallery-two'), data)
+new Gallery(document.querySelector('.gallery'), data)
 /* eslint-enable */
